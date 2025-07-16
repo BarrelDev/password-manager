6cb6049b1eb942b209540df132fe76ce1bcb21c1
Visvam Rajesh <visvam64@gmail.com>
2025-07-16 11:04:59 -0400

    structured data.py to access and write to database

--- data.py
+++ data.py
@@ -1,7 +1,8 @@
 import os
 import io
-import csv
+import pandas as pd
 import base64
+from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
@@ -53,3 +54,41 @@ def get_key(password):
 
     return key
 
+def get_dataframe(password):
+    f = Fernet(get_key(password))
+
+    read_dat = f.decrypt(read_binary_data("data")).decode('utf-8')
+    input = io.StringIO(read_dat)
+    df_read = pd.read_csv(input)
+
+    return df_read
+
+def write_dataframe(password, df):
+    f = Fernet(get_key(password))
+
+    # Save to CSV in-memory using pandas
+    output = io.StringIO()
+    df.to_csv(output, index=False)
+    csv_data = output.getvalue()
+    print(csv_data)
+
+    # Encrypt and save to file
+    token = f.encrypt(csv_data.encode('utf-8'))
+    write_binary_data(token, "data")
+
+def add_service(password, service: str, usrname: str, passwd: str):
+    df = get_dataframe(password)
+    
+    row = [service, usrname, passwd]
+    df = pd.concat([df, pd.DataFrame([row],
+                                     columns=FIELD_NAMES)],
+                   ignore_index=True)
+    
+    write_dataframe(password, df)
+
+def get_credentials(password, service: str):
+    df = get_dataframe(password)
+    row = df.loc[df["service"] == service]
+    if not row.empty:
+        return row.iloc[0]["usrname"], row.iloc[0]["passwd"]
+    return None, None
--- main.py
+++ main.py
@@ -1,33 +1,5 @@
 import data
-import io
-from cryptography.fernet import Fernet
-import pandas as pd
 
 PASSWORD = b"password"
-FIELD_NAMES = ["service", "usrname", "passwd"]
 
-f = Fernet(data.get_key(PASSWORD))
-
-df = pd.DataFrame(columns=FIELD_NAMES)
-df = pd.concat([df, pd.DataFrame([["google", "name", "password"]], columns=FIELD_NAMES)], ignore_index=True)
-
-# Save to CSV in-memory using pandas
-output = io.StringIO()
-df.to_csv(output, index=False)
-csv_data = output.getvalue()
-print(csv_data)
-
-# Encrypt and save to file
-token = f.encrypt(csv_data.encode('utf-8'))
-data.write_binary_data(token, "data")
-
-# Read and decrypt data from file
-read_dat = f.decrypt(data.read_binary_data("data")).decode('utf-8')
-print(read_dat)
-
-# Load back into DataFrame using pandas
-input = io.StringIO(read_dat)
-df_read = pd.read_csv(input)
-
-# Print the dataframe
-print(df_read)
+print(data.get_credentials(PASSWORD, "google"))
