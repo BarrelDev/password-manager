e3223abc439a682d32ba8be553506411e3890936
Visvam Rajesh <visvam64@gmail.com>
2025-07-15 20:43:49 -0400

    added basic encrypt/decrypt functionality

--- data.py
+++ data.py
@@ -1,22 +1,38 @@
 import os
+import io
+import csv
 
 DATA_FOLDER = ".dat/"
+FIELD_NAMES = ["service", "usrname", "passwd"]
 
-def write_binary_data(data, filename):
+def write_binary_data(data, filename: str):
     if os.path.isdir(DATA_FOLDER):
-        with open(DATA_FOLDER + filename, 'xb+') as file:
+        with open(DATA_FOLDER + filename, 'wb+') as file:
             file.write(data)
+            file.close()
     else:
         os.mkdir(DATA_FOLDER)
-        with open(DATA_FOLDER + filename, 'xb+') as file:
+        with open(DATA_FOLDER + filename, 'wb+') as file:
             file.write(data)
+            file.close()
+
+def read_binary_data(filename: str):
+    try:
+        with open(DATA_FOLDER + filename, 'rb') as file:
+            dat = file.read()
+            return dat
+    except:
+        print ('No such file %s exists' % filename)
+        return b''
 
 def get_salt():
     if os.path.exists(DATA_FOLDER+"salt"):
         with open(DATA_FOLDER+"salt", "rb") as file:
-            return file.read()
+            salt = file.read()
+            file.close()
+            return salt
     else:
         salt = os.urandom(16)
         write_binary_data(salt, "salt")
         return salt
-            
+
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import base64
-import os
 import data
+import csv
+import io
+import uuid
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 PASSWORD = b"password"
+FIELD_NAMES = ["service", "usrname", "passwd"]
 
 salt = data.get_salt()
 
@@ -19,8 +22,18 @@ kdf = PBKDF2HMAC(
 key = base64.urlsafe_b64encode(kdf.derive(PASSWORD))
 f = Fernet(key)
 
-data = b"secret message"
-token = f.encrypt(data)
-print (token)
+output = io.StringIO()
+writer = csv.writer(output, quoting=csv.QUOTE_NONNUMERIC)
+writer.writerow(FIELD_NAMES)
+writer.writerow(["google", "name", "password"])
+print(output.getvalue())
+table = output.getvalue()
+token = f.encrypt(table.encode('ascii'))
+
+data.write_binary_data(token, "data")
+
+read_dat = f.decrypt(data.read_binary_data("data")).decode('ascii')
+
+print(read_dat)
 
 
